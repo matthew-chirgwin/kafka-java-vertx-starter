d18782f992488c34353b59c201f0b8c72d45d4bb
Jordan <jordan.tucker1@ibm.com>
2020-06-14 11:29:46 +0100

    test

--- ui/src/Bootstrap/App/App.js
+++ ui/src/Bootstrap/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <div className="App">UI coming soonish!</div>;
+  return <div className="App">UI coming soonishasfasdf!</div>;
 };
 
 export default App;
