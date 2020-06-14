45807d41e0ca3613a9903a1a07d29c81431e78fd
Jordan <jordan.tucker1@ibm.com>
2020-06-14 12:09:27 +0100

    test

--- .github/actions/commenter/index.js
+++ .github/actions/commenter/index.js
@@ -2,17 +2,30 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fs = require("fs");
 
+const pull_request_number = github.context.payload.pull_request.number;
+const repo = github.context.repo;
+const github_token = core.getInput("GITHUB_TOKEN");
+const octokit = new github.GitHub(github_token);
+
+async function getCommentID() {
+  let { data: comments } = await octokit.issues.listComments({
+    ...repo,
+    issue_number: pull_request_number,
+  });
+
+  let res = comments.filter((comment) => {
+    return comment.user.login === "github-actions[bot]";
+  });
+
+  return res[0].id;
+}
+
 function comment(message) {
   try {
-    const github_token = core.getInput("GITHUB_TOKEN");
-
-    const pull_request_number = github.context.payload.pull_request.number;
-    const repo = github.context.repo;
-
-    const octokit = new github.GitHub(github_token);
-    octokit.issues.createComment({
+    octokit.issues.updateComment({
       ...repo,
       issue_number: pull_request_number,
+      comment_id: getCommentID(),
       body: message,
     });
   } catch (error) {
